9661dec54002c678f12ac3de0c33c2843a8a7623
shashank-16 <shashank.chaurasia1606@gmail.com>
2021-05-27 19:07:33 +0530

    dc

---
+++ studentregistration.php
@@ -0,0 +1,22 @@
+<?php
+
+$student_id=$_POST['SID'];
+$student_name=$_POST['F_name'];
+$student_branch=$_POST['branch'];
+$student_section = $_POST['section'];
+
+$connection= mysqli_connect("localhost","root","","shashank") or die("connection failed");
+
+$query= "INSERT INTO student_table(STU_ID,STU_NAME,BRANCH,SECTION)
+VALUES  (
+'{$student_id}','{$student_name}','{$student_branch}','{$student_section}')";
+
+$result=mysqli_query($connection,$query) or die("failed");
+
+header("Location:http://localhost//Github_repo//Website//enroll_student.php");
+
+
+mysqli_close($connection);
+
+
+?>
